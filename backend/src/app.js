c68e7ad2b8435b3290f44ee7ed003b56d6e5e0e4
Jooaomarcelo <joaomdsb@hotmail.com>
2024-10-08 18:31:44 -0300

    added cors habilitado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./router');
 
 const app = express();
 
-// Antes de usar o router -> Função do express para conseguirmos trabalhar com json 
+// Antes de usar o router -> Função do express para conseguirmos trabalhar com json
 app.use(express.json());
 
+// Habilitando o cors para liberar o acesso a aplicação
+app.use(cors());
 app.use(router);
 
 module.exports = app;
